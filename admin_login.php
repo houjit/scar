9dde285bf178bccee1e4a7c2055c2f620aa63dfa
liuloujun <447107108@qq.com>
2019-03-19 10:48:10 +0800

    base

--- admin_login.php
+++ admin_login.php
@@ -14,7 +14,6 @@ include(INC_DIR.'permission.func.php');
 include(INC_DIR . 'html.func.php');
 include ('index/page.php');
 
-
 $m = isset($_GET['m']) ? $_GET['m'] : 'main';
 if (!is_admin_login()) $m = 'login';
 if (!file_exists('admin/'.$m.'.php')) exit('error url');
